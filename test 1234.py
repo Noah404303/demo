c8f43318986683accd3025057adf3b4b4ae6e5c4
Noah404303 <148633577+Noah404303@users.noreply.github.com>
2023-10-21 15:51:56 +0200

    Funktion backen
    erste funktion in python programmirt

--- test 1234.py
+++ test 1234.py
@@ -1,3 +1,7 @@
+def backen(sorte):
+    print("kuche backen: {}".format(sorte))
+
+
 for n in range(1, 10):
     #print("hallo welt: {}".format(n))
     eingabe = input("noah> ") 
@@ -8,7 +12,9 @@ print ("runde 2")
 for n in range(1, 3):
     input("daniel> ")
     
+backen("schocko kuchen")
 print("ende")
 print("byby")
 input("belibige taste")
+
 

088e66758a514402739edc15582fbac4f5692401
Noah404303 <148633577+Noah404303@users.noreply.github.com>
2023-10-21 15:37:58 +0200

    Beispiel 12345.py
    Demonstration von ersten Version des codes

---
+++ test 1234.py
@@ -0,0 +1,14 @@
+for n in range(1, 10):
+    #print("hallo welt: {}".format(n))
+    eingabe = input("noah> ") 
+    print("NACHRICHT: {}".format(eingabe))
+
+
+print ("runde 2")
+for n in range(1, 3):
+    input("daniel> ")
+    
+print("ende")
+print("byby")
+input("belibige taste")
+
